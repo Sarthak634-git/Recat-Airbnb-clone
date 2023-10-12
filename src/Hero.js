2b4308c7399eda6140260876ad588904cfe4e3e7
Sarthak Thapa <sarthakthapa634@gmail.com>
2023-10-12 22:08:08 +0545

    Card prototype.

--- src/Hero.js
+++ src/Hero.js
@@ -7,11 +7,55 @@ export default function Hero() {
       <section>
         <img src={ima} className='image22' alt="ima" />
         <h1 className="hero--heading">
-            Online Expiriance
+          Online Expiriance
         </h1>
         <p className="hero--text">
-            Join our unique interactive led by one of a kind hosts all without leaving home.
+          Join our unique interactive led by one of a kind hosts all without leaving home.
         </p>
+        <div className="card">
+          <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">An item</li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">An item</li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">An item</li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-body">
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+        </div>
       </section>
     </div>
   )
